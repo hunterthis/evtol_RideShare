563c4c4434b4a76fd400fd65c8e3ded49684c6d7
Hunter <46310167+hunterthis@users.noreply.github.com>
2022-11-28 12:35:36 -0800

    added create view for pilots, removed private readonly redundant value for pilot data storage

--- Models/Pilot.cs
+++ Models/Pilot.cs
@@ -3,22 +3,18 @@ namespace evtol_RideShare.Models
 {
     public class Pilot
     {
-        private readonly string? FAATrackingNumber;
-
         [Key]
         public int FTN { get; set; }
         public string? Name { get; set; }
         /// <summary>
         /// Federal Aviation Administration
         /// </summary>
-       // public string FAA tracking number (FTN) { get => FTN; set => FTN = value; }
-        // TO DO: implement FAA number restrictions
     }
 
         
 
     /// <summary>
-    /// Creates cookies model for passengers to retrieve pilot information including FAA Number 
+    /// Creates cookies model for pilots storing FAA tracking number as int and Name combo
     /// </summary>
 
 }
